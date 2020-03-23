15b572890b81216d4355c8ada84c5923c7cd72f7
Jafet438 <62528555+Jafet438@users.noreply.github.com>
2020-03-22 20:31:33 -0600

    Add files via upload

---
+++ practicapython.py
@@ -0,0 +1,23 @@
+print(#1)
+for x in range(1,8):
+	print("*" * x)
+	print()
+
+print(#2)
+for y in range(8,0,-1):
+	print("*" * y)
+print()
+
+print(#3)
+for m in range(1,8):
+	print("*" * m)
+print()
+for n in range(8,0,-1):
+	print("*" * n)
+print()
+
+print(#4)
+for z in range(1,8):
+	print("*" * 8)
+print()
+		
